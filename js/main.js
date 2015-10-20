13fdfcacbe8fd6704db82c4fb10fdf87c43f79d2
jiangyulin <hello.wenzhe.com>
2015-10-20 13:34:59 +0800

    new file:   css/ie8.css 	modified:   css/main.css 	modified:   index.html 	modified:   js/jquery.min.js 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -2,139 +2,168 @@
  * Created by JiangYulin on 2015/10/14.
  */
 
-
+console.log(navigator.userAgent)
+var IE8 = 1;
+var IE = 0;
+if (navigator.userAgent.indexOf('MSIE 8') >= 0) {
+    IE8 = 1;
+}
+else {
+    IE8 = 0;
+}
+if (navigator.userAgent.indexOf('MSIE 9') >= 0) {
+    IE = 1;
+}
+else {
+    IE = 0;
+}
 window.onload = function()
 {
-    /* for webkit */
-    document.body.addEventListener("mousewheel",function(event){
-        /*
-        need a lock for 0.7s
-         */
-        if(islock())
-        {
-            return false;
-        }
-        lock();
-        if(event.deltaY > 0)
-        {
-            goNext();
-            setTimeout(unlock,700);
-        }
-        else {
-            goPrev();
-            setTimeout(unlock,700);
-        }
-    });
-    /* for firefox */
-    document.body.addEventListener("DOMMouseScroll",function(event){
-        /*
-         need a lock for 0.7s
-         */
-        if(islock())
-        {
-            return false;
-        }
-        lock();
-        if(event.detail > 0)
-        {
-            goNext();
-            setTimeout(unlock,700);
-        }
-        else {
-            goPrev();
-            setTimeout(unlock,700);
-        }
-    });
+    if(!IE8) {
+        /* for webkit */
+        document.body.addEventListener("mousewheel", function (event) {
+            /*
+             need a lock for 0.7s
+             */
+            if (islock()) {
+                return false;
+            }
+            lock();
+            if (event.wheelDelta > 0) {
+                goNext();
+                setTimeout(unlock, 700);
+            }
+            else {
+                goPrev();
+                setTimeout(unlock, 700);
+            }
+        });
+        /* for firefox */
+        document.body.addEventListener("DOMMouseScroll", function (event) {
+            /*
+             need a lock for 0.7s
+             */
+            if (islock()) {
+                return false;
+            }
+            lock();
+            if (event.detail > 0) {
+                goNext();
+                setTimeout(unlock, 700);
+            }
+            else {
+                goPrev();
+                setTimeout(unlock, 700);
+            }
+        });
+    } else {
+        window.attachEvent("mousewheel", function (event) {
+            /*
+             need a lock for 0.7s
+             */
+            if (islock()) {
+                return false;
+            }
+            lock();
+            if (event.wheelDelta > 0) {
+                goNext();
+                setTimeout(unlock, 700);
+            }
+            else {
+                goPrev();
+                setTimeout(unlock, 700);
+            }
+        });
+    }
 
-    /* particlesJS('dom-id', params);
-     /* @dom-id : set the html tag id [string, optional, default value : particles-js]
-     /* @params: set the params [object, optional, default values : check particles.js] */
+    if(!IE8) {
+        /* particlesJS('dom-id', params);
+         /* @dom-id : set the html tag id [string, optional, default value : particles-js]
+         /* @params: set the params [object, optional, default values : check particles.js] */
 
-    /* config dom id (optional) + config particles params */
-    particlesJS('particles-js', {
-        particles: {
-            color: '#fff',
-            shape: 'triangle', // "circle", "edge" or "triangle"
-            opacity: 1,
-            size: 2,
-            size_random: true,
-            nb: 50,
-            line_linked: {
-                enable_auto: true,
-                distance: 50,
+        /* config dom id (optional) + config particles params */
+        particlesJS('particles-js', {
+            particles: {
                 color: '#fff',
+                shape: 'triangle', // "circle", "edge" or "triangle"
                 opacity: 1,
-                width: 1,
-                condensed_mode: {
-                    enable: false,
-                    rotateX: 600,
-                    rotateY: 600
-                }
-            },
-            anim: {
-                enable: true,
-                speed: 8
-            }
-        },
-        interactivity: {
-            enable: true,
-            mouse: {
-                distance: 250
-            },
-            detect_on: 'canvas', // "canvas" or "window"
-            mode: 'grab',
-            line_linked: {
-                opacity: .5
-            },
-            events: {
-                onclick: {
+                size: 2,
+                size_random: true,
+                nb: 50,
+                line_linked: {
+                    enable_auto: true,
+                    distance: 50,
+                    color: '#fff',
+                    opacity: 1,
+                    width: 1,
+                    condensed_mode: {
+                        enable: false,
+                        rotateX: 600,
+                        rotateY: 600
+                    }
+                },
+                anim: {
                     enable: true,
-                    mode: 'push', // "push" or "remove" (particles)
-                    nb: 4
+                    speed: 8
                 }
-            }
-        },
-        /* Retina Display Support */
-        retina_detect: true
-    });
+            },
+            interactivity: {
+                enable: true,
+                mouse: {
+                    distance: 250
+                },
+                detect_on: 'canvas', // "canvas" or "window"
+                mode: 'grab',
+                line_linked: {
+                    opacity: .5
+                },
+                events: {
+                    onclick: {
+                        enable: true,
+                        mode: 'push', // "push" or "remove" (particles)
+                        nb: 4
+                    }
+                }
+            },
+            /* Retina Display Support */
+            retina_detect: true
+        });
 
-    /*
+        /*
          auto zoom
-     */
-    {
-        _resize();
-        $(window).resize(function () {
+         */
+        {
             _resize();
+            $(window).resize(function () {
+                _resize();
+            })
+        }
+        //auto zoom 结束
+        /*
+         Ipad 滑动支持
+         */
+        fixed_touch_support();
+
+        /*
+         contact 部分的表单提交
+         */
+        //验证表单
+        _validateOnBlur($("#contact-form"));
+        //验证并提交
+        $("#contact-form-submit").click(function (event) {
+            event.preventDefault();
+            if (_validateForm($("#contact-form"))) {
+                $("#contact-form").ajaxSubmit({
+                    success: function (data) {
+                        if (data == 0) {
+                        }
+                        else {
+                        }
+                    }
+                });
+            }
         })
     }
-    //auto zoom 结束
-    /*
-    Ipad 滑动支持
-     */
-    fixed_touch_support();
-
-    /*
-        contact 部分的表单提交
-    */
-    //验证表单
-    _validateOnBlur($("#contact-form"));
-    //验证并提交
-    $("#contact-form-submit").click(function(event){
-        event.preventDefault();
-        if(_validateForm($("#contact-form")))
-        {
-            $("#contact-form").ajaxSubmit({
-                success:function(data){
-                    if(data == 0)
-                    {
-                    }
-                    else
-                    {
-                    }
-                }
-            });
-        }
-    })
 
 };
 /*
@@ -226,14 +255,14 @@ function _resize() {
             /* 以宽度比例计算 */
             var Q = current_W_W/origin.width;
             console.log("Q=" + Q);
-            $(this).css("zoom",Q);
+            $(this).css("transform","scale("+Q+")");
         }
         else
         {
             /* 以高度比例计算*/
             var Q = current_W_H/origin.height;
             console.log("Q=" + Q);
-            $(this).css("zoom",Q);
+            $(this).css("transform","scale("+Q+")");
         }
     });
 }
@@ -269,8 +298,6 @@ $(function(){
 
  */
 
-
-
 /*
 lock & unlock
  */
@@ -298,24 +325,44 @@ function goPrev()
     var front_section = $("#showcase").find(".active");
     var prev_front_section = front_section.prev();
     if(prev_front_section.length != 0) {
-        front_section.removeClass("active");
-        front_section.removeClass("_front-section");
-        prev_front_section.addClass("active");
+        if(IE) {
+            front_section.animate({'opacity': 0}, function () {
+                front_section.removeClass("_front-section");
+                front_section.removeClass("active");
+                prev_front_section.addClass("active");
+                front_section.css("opacity", 1);
+            })
+        } else {
+            front_section.removeClass("_front-section");
+            front_section.removeClass("active");
+            prev_front_section.addClass("active");
+        }
     }
 }
 
 /*
 slide to next page
  */
+
 function goNext()
 {
     var front_section = $("#showcase").find(".active");
     var next_front_section = front_section.next();
     if(next_front_section.length != 0) {
-        next_front_section.addClass("active");
-        front_section.removeClass("active");
-        next_front_section.addClass("_front-section");
-        front_section.addClass("_front-section");
+        if(IE) {
+            next_front_section.addClass("_front-section");
+            next_front_section.css({"opacity":0});
+            next_front_section.animate({"opacity": 1}, function () {
+                front_section.removeClass("active");
+                front_section.addClass("_front-section");
+                next_front_section.addClass("active");
+            })
+        } else {
+                next_front_section.addClass("active");
+                next_front_section.addClass("_front-section");
+                front_section.removeClass("active");
+                front_section.addClass("_front-section");
+        }
     }
 }
 
